2541d75d0c40268398bc78fcce92eb1d0f34d0b9
Drenos Achilles <45300759+achilleas97@users.noreply.github.com>
2021-01-27 17:38:10 +0200

    Create Parameter.h

---
+++ Parameter.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID ""           //Your SSID
+#define SECRET_PASS ""           //Your Password
+#define SECRET_TOKEN ""          //Your Token SmartFarm
+#define BOTtoken ""              // your Bot Token (Get from Botfather)
+#define CHAT_ID ""
